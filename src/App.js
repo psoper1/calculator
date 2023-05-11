e74772f4d686d8e3652574fa9d9c735b2ebb4bc8
psoper1 <psoper0604@gmail.com>
2023-05-11 08:25:01 -0400

    made look nicer

--- src/App.js
+++ src/App.js
@@ -5,36 +5,36 @@ function App() {
   const [displayValue, setDisplayValue] = useState('0');
 
   const handleClick = (value) => {
-    setDisplayValue(displayValue === '0' ? value : displayValue + value);
+    setDisplayValue(displayValue === '0' ? value : displayValue + '' + value);
   };
 
   return (
     <>
     <div className='container mainview'>
-    <div className='display justify-content-center'>{displayValue}</div>
+    <div className='display'>{displayValue}</div>
       <div className='col col-4'>
-    <button onClick={() => handleClick('9')}>9</button>
-    <button onClick={() => handleClick('8')}>8</button>
-    <button onClick={() => handleClick('7')}>7</button>
-    <button onClick={() => setDisplayValue('0')}>C</button>
+    <button className='buttons' onClick={() => handleClick('9')}>9</button>
+    <button className='buttons' onClick={() => handleClick('8')}>8</button>
+    <button className='buttons' onClick={() => handleClick('7')}>7</button>
+    <button className='buttons' onClick={() => setDisplayValue('0')}>C</button>
     </div>
     <div className='col col-4'>
-    <button onClick={() => handleClick('6')}>6</button>
-    <button onClick={() => handleClick('5')}>5</button>
-    <button onClick={() => handleClick('4')}>4</button>
-    <button onClick={() => handleClick('/')}>/</button>
+    <button className='buttons' onClick={() => handleClick('6')}>6</button>
+    <button className='buttons' onClick={() => handleClick('5')}>5</button>
+    <button className='buttons' onClick={() => handleClick('4')}>4</button>
+    <button className='buttons' onClick={() => handleClick('/')}>/</button>
     </div>
     <div className='col col-4'>
-    <button onClick={() => handleClick('3')}>3</button>
-    <button onClick={() => handleClick('2')}>2</button>
-    <button onClick={() => handleClick('1')}>1</button>
-    <button onClick={() => handleClick('*')}>*</button>
+    <button className='buttons' onClick={() => handleClick('3')}>3</button>
+    <button className='buttons' onClick={() => handleClick('2')}>2</button>
+    <button className='buttons' onClick={() => handleClick('1')}>1</button>
+    <button className='buttons' onClick={() => handleClick('*')}>*</button>
     </div>
     <div className='col col-4'>
-    <button className='justify-content-center' onClick={() => handleClick('0')}>0</button>
-    <button onClick={() => handleClick('+')}>+</button>
-    <button onClick={() => handleClick('-')}>-</button>
-    <button onClick={() => setDisplayValue(eval(displayValue))}>=</button>
+    <button className='buttons justify-content-center' onClick={() => handleClick('0')}>0</button>
+    <button className='buttons' onClick={() => handleClick('+')}>+</button>
+    <button className='buttons' onClick={() => handleClick('-')}>-</button>
+    <button className='buttons' onClick={() => setDisplayValue(eval(displayValue))}>=</button>
     </div>
     </div>
     </>
